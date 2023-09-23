2258ee9689b04add8e0ce7d1c9e91055ba7562d0
Masonord <trotf233@gmail.com>
2023-09-23 21:08:59 +0200

    refactoring: fix some issues with jest

---
+++ jest.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "@jest/types"
+
+const config: Config.InitialOptions = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  modulePathIgnorePatterns: ["/dist/test"],
+  verbose: true,
+  automock: false,
+}
+export default config
