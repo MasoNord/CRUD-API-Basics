1ae865a594544ebfeb46523fdaac2e1fcebd4ab4
Masonord <trotf233@gmail.com>
2023-09-26 13:40:46 +0200

    refactoring: update tests

--- test/users.e2e.spec.ts
+++ test/users.e2e.spec.ts
@@ -2,31 +2,28 @@ import { StatusCodes } from './interface/http.status.codes';
 import { AppController } from '../src/app/app.controller';
 import { UserController } from '../src/app/user/user.controller';
 import { UserService } from '../src/app/user/users.service';
-import { User } from '../src/app/user/entities/user.entity';
-import request from 'supertest';
+import { pool } from '../src/database';
+import request, { agent } from 'supertest';
+import { port } from '../src/config';
 
 const server = new AppController(new UserController(new UserService())).getSever();
 
-const testId = 'a94fd38c-d30d-40a3-9b52-af324e5ffa7f';
 const dummyId = '8a28c588-466a-4a2d-ad7c-41e0385a35e2';
 const wrongId = '466a-4a2d';
 
 const createUser = {
-  id: testId,
   username: 'Molly',
   age: 19,
   hobbies: ['Reading', 'Swimming', 'Listening music', 'Walking on fresh air'],
 };
 
 const createDummyUser = {
-  id: testId,
   username: 138420432,
   age: 19,
   hobbies: [],
 };
 
 const createLiteUser = {
-  id: testId,
   username: 'Donny',
 };
 
@@ -36,10 +33,22 @@ const updateUserData = {
 
 const incorrectUpdateUserData = {
   age: '32',
+  username: 'Karl',
+  hobbies: [],
 };
 
 describe('USERS', () => {
   const commonHeaders = { Accept: 'application/json' };
+  let app;
+  beforeEach(() => {
+    app = server.listen(port, () => {
+      global.agent = request.agent(server);
+    });
+  });
+
+  afterEach(() => {
+    app.close();
+  });
 
   describe('GET ', () => {
     it('should correctly get all users', async () => {
@@ -49,21 +58,22 @@ describe('USERS', () => {
     });
     it('should correctly get user by id', async () => {
       const creationResponse = await request(server).post('/api/users').set(commonHeaders).send(createUser);
-      const { id } = creationResponse.body;
+      const id: string = creationResponse.body.newUser.id;
+      const response = await request(server).get(`/api/users/${id}`).set(commonHeaders);
+
+      const deleteResponse = await request(server).delete(`/api/users/${id}`).set(commonHeaders);
       expect(creationResponse.status).toBe(StatusCodes.CREATED);
 
-      const response = await request(server).get(`/api/users/${id}`).set(commonHeaders);
       expect(response.status).toBe(StatusCodes.OK);
       expect(response.body).toBeInstanceOf(Object);
+
+      expect(deleteResponse.status).toBe(StatusCodes.NO_CONTENT);
     });
     it("should perform bad request in case if was recived wrong id's format", async () => {
       const response = await request(server).get(`/api/users/${wrongId}`).set(commonHeaders);
       expect(response.status).toBe(StatusCodes.BAD_REQUEST);
     });
     it('should perform not found request in case if record does not exist', async () => {
-      const creationResponse = await request(server).post('/api/users').set(commonHeaders).send(createUser);
-      expect(creationResponse.status).toBe(StatusCodes.CREATED);
-
       const response = await request(server).get(`/api/users/${dummyId}`).set(commonHeaders);
       expect(response.status).toBe(StatusCodes.NOT_FOUND);
     });
@@ -72,14 +82,20 @@ describe('USERS', () => {
   describe('POST', () => {
     it("should correctly create new user's record", async () => {
       const creationResponse = await request(server).post('/api/users').set(commonHeaders).send(createUser);
+      const id: string = creationResponse.body.newUser.id;
+      const deleteResponse = await request(server).delete(`/api/users/${id}`).set(commonHeaders);
+
       expect(creationResponse.status).toBe(StatusCodes.CREATED);
-      expect(creationResponse.body).toBeInstanceOf(Object);
+      expect(deleteResponse.status).toBe(StatusCodes.NO_CONTENT);
     });
     it('should perform bad request in case if was recived invalid data types', async () => {
-      const creationResponse = await request(server).post('/api/users').set(commonHeaders).send(createDummyUser);
+      const creationResponse = await request(server)
+        .post('/api/users')
+        .set(commonHeaders)
+        .send(incorrectUpdateUserData);
       expect(creationResponse.status).toBe(StatusCodes.BAD_REQUEST);
     });
-    it('should perform bad request in case if was re not all fields', async () => {
+    it('should perform bad request in case if was filled not all fields', async () => {
       const creationResponse = await request(server).post('/api/users').set(commonHeaders).send(createLiteUser);
       expect(creationResponse.status).toBe(StatusCodes.BAD_REQUEST);
     });
@@ -88,42 +104,35 @@ describe('USERS', () => {
   describe('PUT', () => {
     it("should correctly change user's data", async () => {
       const creationResponse = await request(server).post('/api/users').set(commonHeaders).send(createUser);
-
-      const { id } = creationResponse.body;
-
-      expect(creationResponse.status).toBe(StatusCodes.CREATED);
+      const id: string = creationResponse.body.newUser.id;
 
       const updateResponse = await request(server).put(`/api/users/${id}`).set(commonHeaders).send(updateUserData);
-      expect(updateResponse.status).toBe(StatusCodes.OK);
-      expect(updateResponse.body).toBeInstanceOf(Object);
-    });
-    it('should perform not found request in case if was recived non existing id', async () => {
-      const creationResponse = await request(server).post('/api/users').set(commonHeaders).send(createUser);
+      const deleteResponse = await request(server).delete(`/api/users/${id}`).set(commonHeaders);
+
       expect(creationResponse.status).toBe(StatusCodes.CREATED);
 
-      const updateResponse = await request(server).put(`/api/users/${dummyId}`).set(commonHeaders).send(updateUserData);
+      expect(updateResponse.status).toBe(StatusCodes.OK);
+
+      expect(deleteResponse.status).toBe(StatusCodes.NO_CONTENT);
+    });
+    it('should perform not found request in case if was recived non existing id', async () => {
+      const updateResponse = await request(server).put(`/api/users/${dummyId}`).set(commonHeaders);
       expect(updateResponse.status).toBe(StatusCodes.NOT_FOUND);
     });
     it('should perform bad request in case if was recived ivalid data types', async () => {
       const creationResponse = await request(server).post('/api/users').set(commonHeaders).send(createUser);
-      expect(creationResponse.status).toBe(StatusCodes.CREATED);
-
-      const { id } = creationResponse.body;
-
+      const id: string = creationResponse.body.newUser.id;
       const updateResponse = await request(server)
         .put(`/api/users/${id}`)
         .set(commonHeaders)
         .send(incorrectUpdateUserData);
+
+      expect(creationResponse.status).toBe(StatusCodes.CREATED);
+
       expect(updateResponse.status).toBe(StatusCodes.BAD_REQUEST);
     });
     it("should perform bad request in case if was recived wrong id's format", async () => {
-      const creationResponse = await request(server).post('/api/users').set(commonHeaders).send(createUser);
-      expect(creationResponse.status).toBe(StatusCodes.CREATED);
-
-      const updateResponse = await request(server)
-        .put(`/api/users/${wrongId}`)
-        .set(commonHeaders)
-        .send(incorrectUpdateUserData);
+      const updateResponse = await request(server).put(`/api/users/${wrongId}`).set(commonHeaders);
       expect(updateResponse.status).toBe(StatusCodes.BAD_REQUEST);
     });
   });
@@ -131,34 +140,19 @@ describe('USERS', () => {
   describe('DELETE', () => {
     it("should correclty delete user's record", async () => {
       const creationResponse = await request(server).post('/api/users').set(commonHeaders).send(createUser);
+      const id: string = creationResponse.body.newUser.id;
+      const deleteResponse = await request(server).delete(`/api/users/${id}`).set(commonHeaders);
+
       expect(creationResponse.status).toBe(StatusCodes.CREATED);
 
-      const { id } = creationResponse.body;
-
-      const deleteResponse = await request(server)
-        .delete(`/api/users/${id}`)
-        .set(commonHeaders)
-        .send(incorrectUpdateUserData);
       expect(deleteResponse.status).toBe(StatusCodes.NO_CONTENT);
     });
     it('should perform bad request in case if was recived wrong id format', async () => {
-      const creationResponse = await request(server).post('/api/users').set(commonHeaders).send(createUser);
-      expect(creationResponse.status).toBe(StatusCodes.CREATED);
-
-      const deleteResponse = await request(server)
-        .delete(`/api/users/${wrongId}`)
-        .set(commonHeaders)
-        .send(incorrectUpdateUserData);
+      const deleteResponse = await request(server).delete(`/api/users/${wrongId}`).set(commonHeaders);
       expect(deleteResponse.status).toBe(StatusCodes.BAD_REQUEST);
     });
     it('should perform not found request in case if was recived non existing id', async () => {
-      const creationResponse = await request(server).post('/api/users').set(commonHeaders).send(createUser);
-      expect(creationResponse.status).toBe(StatusCodes.CREATED);
-
-      const deleteResponse = await request(server)
-        .delete(`/api/users/${dummyId}`)
-        .set(commonHeaders)
-        .send(incorrectUpdateUserData);
+      const deleteResponse = await request(server).delete(`/api/users/${dummyId}`).set(commonHeaders);
       expect(deleteResponse.status).toBe(StatusCodes.NOT_FOUND);
     });
   });
